d8eaddc91aee6b5b5c78f91107da1ea7170ae13a
Ivan Consalter <ivanconsalter1@gmail.com>
2021-11-16 08:38:39 -0300

    fixed bug in shared/shared.module.ts

--- app-my-finances/src/app/shared/shared.module.ts
+++ app-my-finances/src/app/shared/shared.module.ts
@@ -2,17 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReactiveFormsModule } from '@angular/forms';
-import { BaseResourceListComponent } from './components/base-resource-list/base-resource-list/base-resource-list.component';
 
 @NgModule({
-  declarations: [BaseResourceListComponent],
+  declarations: [],
   imports: [
     CommonModule,
     ReactiveFormsModule
   ],
   exports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ]
 })
 export class SharedModule { }
