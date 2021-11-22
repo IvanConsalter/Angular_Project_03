2c4844f413e728390c50af4069dd69ac38422362
Ivan Consalter <ivanconsalter1@gmail.com>
2021-11-22 09:08:12 -0300

    extrating navbar.component

--- app-my-finances/src/app/core/core.module.ts
+++ app-my-finances/src/app/core/core.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -7,21 +8,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDatabase } from '../in-memory-database';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
+    NavbarComponent
   ],
   imports: [
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
+    RouterModule
   ],
   exports: [
+    // core modules
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    // core components
+    NavbarComponent
   ]
 })
 export class CoreModule { }
